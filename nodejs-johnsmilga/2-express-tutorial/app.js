85f294bfb0f51d1435633e52fcf77951b861b4ba
akifkiraz <69064758+akifk11@users.noreply.github.com>
2023-08-02 06:31:30 +0300

    req.params

--- nodejs-johnsmilga/2-express-tutorial/app.js
+++ nodejs-johnsmilga/2-express-tutorial/app.js
@@ -1,9 +1,26 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+const { products } = require("./data");
+app.get("/", (req, res) => {
+  res.send(`<h1>Home Page</h1><a href="/api/products">profucts</a>`);
+});
+app.get("/api/products/:productID", (req, res) => {
+  //   const newProducts = products.map((product) => {
+  //     const { id, name, image } = product;
+  //     return { id, name, image };
+  //   console.log(req);
+  // console.log(req.params);
+  const { productID } = req.params;
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res.status(404).send("Product does not exist ");
+  }
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/html" });
-  res.write("<h1>home page</h1>");
-  res.end();
+  res.json(singleProduct);
 });
 
-server.listen(5000);
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000....");
+});
---
+++ nodejs-johnsmilga/2-express-tutorial/my-exercise-folder/02-http-app.js
@@ -0,0 +1,50 @@
+const http = require("http");
+const { readFileSync } = require("fs");
+
+const homePage = readFileSync("./navbar-app/index.html");
+const homeStyles = readFileSync("./navbar-app/styles.css");
+const homeLogo = readFileSync("./navbar-app/logo.svg");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
+
+const server = http.createServer((req, res) => {
+  //console.log(req.method);
+  //console.log(req.url);
+  const url = req.url;
+  console.log(url);
+  // Home page
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(homePage);
+    res.end();
+  }
+  // About page
+  else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>about page</h1>");
+    res.end();
+  }
+  // styles
+  else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homeStyles);
+    res.end();
+  }
+  // image/logo
+  else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(homeLogo);
+    res.end();
+  }
+  // logic
+  else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.write(homeLogic);
+    res.end();
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>page not found</h1>");
+    res.end();
+  }
+});
+
+server.listen(5000);
